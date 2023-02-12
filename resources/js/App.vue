e7be9b947b7f5e0b8d7db25f5bc978f49722ef68
xLink <xLink@cybershade.org>
2023-02-12 16:19:35 +0000

    FUCKKKKK YOUUUU

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <div class="font-red-500">test</div>
+    <div class="text-red-500">test</div>
+    <div class="flex flex-col">
+      <dix class="flex bg-red-200">a</dix>
+      <dix class="flex bg-gray-600">b</dix>
+    </div>
     <HelloWorld msg="Hello Vue 2 + Vite" />
   </div>
 </template>
