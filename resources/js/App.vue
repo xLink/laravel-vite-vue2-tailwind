b895949d18855be0aedeceee35888106ccb86586
xLink <xLink@cybershade.org>
2023-02-12 16:04:41 +0000

    init

---
+++ resources/js/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div class="font-red-500">test</div>
+    <HelloWorld msg="Hello Vue 2 + Vite" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+export default {
+  components: {
+    HelloWorld,
+  },
+};
+</script>
---
+++ resources/views/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Core API Documentation</title>
+
+@vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="font-open-sans leading-none antialiased">
+<div id="app"></div>
+</body>
+</html>
